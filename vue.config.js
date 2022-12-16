8e1ca5d91ce7a6abd5756229e3bc46987424be6b
Alexander <Starnovskiy97@gmail.com>
2022-12-16 19:18:26 +0900

    Updated vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/Registration-page/'
+  publicPath: '/Auth-page/'
 })
